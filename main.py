59e5482382c71911015d5e96537032f15c792763
Faruk <farukmd494@gmail.com>
2021-01-22 18:12:42 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+imgl='C:/Users/faruk/Desktop/'
+filename='faruk.jpg'
+
+img=cv2.imread(imgl+filename)
+
+gray_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+inverted_gray_img=255-gray_img
+blurred_img=cv2.GaussianBlur(inverted_gray_img,(21,21),0)
+inverted_blurred_img=255-blurred_img
+pensil_img=cv2.divide(gray_img,inverted_blurred_img,scale=256.0)
+
+
+cv2.imshow('orginal image',img)
+cv2.imshow('New image',pensil_img)
+cv2.waitKey(0)
