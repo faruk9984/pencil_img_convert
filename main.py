7195f838744e2e28e21b14597c9970f4c98c49b9
Mohammad Faruk <40043055+faruk9984@users.noreply.github.com>
2021-01-22 18:15:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ gray_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 inverted_gray_img=255-gray_img
 blurred_img=cv2.GaussianBlur(inverted_gray_img,(21,21),0)
 inverted_blurred_img=255-blurred_img
-pensil_img=cv2.divide(gray_img,inverted_blurred_img,scale=256.0)
+pencil_img=cv2.divide(gray_img,inverted_blurred_img,scale=256.0)
 
 
-cv2.imshow('orginal image',img)
-cv2.imshow('New image',pensil_img)
+cv2.imshow('original image',img)
+cv2.imshow('New image',pencil_img)
 cv2.waitKey(0)
